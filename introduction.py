b35d0e4e8d39c291bb84767e78eb942e404cdd33
Nauman Arif <naumanarif21@gmail.com>
2021-02-24 11:41:05 +0530

    first commit

---
+++ introduction.py
@@ -0,0 +1,12 @@
+"""
+    Python - Basics & Beyond
+
+    This is the start of the Python Training 
+    that NullStack Technologies is giving to colleges.
+
+    Organizers: Sayema Fatema
+                Nauman Arif
+    Dated: 24/02/2021
+    Website: nullstacks.com
+    Email: null@nullstacks.com
+"""
